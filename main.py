594be3eacbcf5c86b77d35d94e6f5fb628562fb7
jbarranco99 <161100490+jbarranco99@users.noreply.github.com>
2024-02-25 22:29:09 -0500

    Update main.py

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def index():
     return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
 
 
-@app.route('/test', methods=['POST'])
+@app.route('/test', methods=['GET','POST'])
 def process_data():
     # Parse JSON from the request
     req_data = request.get_json()
