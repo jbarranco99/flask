9a088e380249f344b4f37d0ea26efe318b0d4016
jbarranco99 <161100490+jbarranco99@users.noreply.github.com>
2024-03-06 01:10:34 -0500

    Update main.py

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ def process_data():
     pendingCategories = req_data['pendingCategories']
     userInput = req_data['userInput']
     selection_paths = req_data.get('selection_paths', [])
-    selection_paths = convert_selection_paths(selection_paths)
+    selection_paths = convert_selection_paths(selection_paths) ## CAMBIAR A SOLO CUANDO VA A DISHPICKER
     game_started = req_data['game_started']
     menu_data = req_data.get('menu', {})  # The complete menu data
     answers = []
