fba0a2d10a1f42f5622b1cdbd7088d27f525000f
jbarranco99 <161100490+jbarranco99@users.noreply.github.com>
2024-04-18 18:18:37 -0400

    Update main.py
    STABLE, the other was not

--- main.py
+++ main.py
@@ -17,62 +17,23 @@ def get_value(d, path):
     return d
 
 
-def string_paths_to_lists(string_paths, delimiter='/'):
-    """
-    Convert a list of string paths to a list of lists of keys.
-    """
-    return [path.split(delimiter) for path in string_paths]
-
-
-def paths_to_string(paths, delimiter='/'):
-    """
-    Convert each path in paths to a string using the given delimiter.
-    """
-    return [delimiter.join(path) for path in paths]
-
-
-# Path filtering functions
-def filter_complete_paths(paths):
-    simplified_paths = [[item for item in path if item.lower() != "subcategories"] for path in paths]
-    complete_paths = filter_paths_with_all_ancestors(simplified_paths)
-    terminal_paths = filter_for_terminal_paths(complete_paths)
-    return terminal_paths
-
-
-def filter_paths_with_all_ancestors(simplified_paths):
-    paths_as_tuples_set = set(tuple(path) for path in simplified_paths)
-    complete_paths_tuples = [path for path in paths_as_tuples_set if
-                             immediate_ancestor_present(path, paths_as_tuples_set)]
-    complete_paths_lists = [list(path) for path in complete_paths_tuples]
-    return ensure_base_paths(complete_paths_lists)
-
-
-def immediate_ancestor_present(path, all_paths_set):
-    if len(path) == 1:
-        return True
-    immediate_ancestor = path[:-1]
-    return tuple(immediate_ancestor) in all_paths_set
-
-
-def ensure_base_paths(complete_paths_lists):
-    for path in complete_paths_lists:
-        if len(path) > 1:
-            base_path = path[:2]
-            if base_path not in complete_paths_lists:
-                complete_paths_lists.append(base_path)
-    return complete_paths_lists
-
-
-def filter_for_terminal_paths(paths):
-    terminal_paths = [path for path in paths if not any(is_prefix(path, other) for other in paths if path != other)]
-    return terminal_paths
-
-
-def is_prefix(path, other_path):
-    if len(path) >= len(other_path):
-        return False
-    return all(path[i] == other_path[i] for i in range(len(path)))
-
+def find_levels(data, target_values, current_path=None, results=None):
+    """Recursively find and record levels of target values in a nested structure."""
+    if current_path is None:
+        current_path = []
+    if results is None:
+        results = []
+    if isinstance(data, dict):
+        for key, value in data.items():
+            if key in target_values:
+                results.append(('Key', key, current_path))
+            find_levels(value, target_values, current_path + [key], results)
+    elif isinstance(data, list):
+        for index, item in enumerate(data):
+            if item in target_values:
+                results.append(('Value', item, current_path + [str(index)]))
+            find_levels(item, target_values, current_path + [str(index)], results)
+    return results
 
 @app.route('/menuToFullTree', methods=['POST'])
 def menuToFullTree():
@@ -169,14 +130,24 @@ def process_data():
         
         pendingcat1.pop(0)
         pendingCategories.extend(answers)
+        
+
 
     else:
-        for category in userInput:
-            current_answers = get_value(data, ['subcategories', category, 'names'])
-            if current_answers:
-                answers.extend(current_answers)
-                selection_path = ['subcategories', category]
-                selection_paths.append(selection_path)
+        results = find_levels(data, userInput)
+        filtered_results = [result for result in results if result[0] == 'Key']
+
+        for result in filtered_results:
+            _, value, path = result
+            full_path = path + [value, 'names']
+            current_answers = get_value(data, full_path)
+            if current_answers is not None:
+                if isinstance(current_answers, list):
+                    answers.extend(current_answers)
+                else:
+                    answers.append(current_answers)
+                # Update selection_paths with the current path
+                selection_paths.append(full_path[:-1])  # Exclude 'names' from the path
 
     # Combine allowed values: pendingcat1, user_input, and answers
     allowed_values = set(pendingcat1 + answers)
@@ -184,12 +155,14 @@ def process_data():
     # Update pending_categories to include only allowed values, and add new answers to the start
     pending_categories = [item for item in answers + pendingcat1]
 
-    if len(pendingcat1) == 0 and len(pending_categories) == 0:
+    if len(pendingcat1) == len(pending_categories):
         gameStage = "dishPicker"
         terminal_paths = filter_complete_paths(selection_paths)
         # Traverse each path to find and accumulate the corresponding items
+        # Assuming 'menu_data' is your complete menu structure
+        # and 'terminal_paths' are the paths you've determined to traverse:
         for path in terminal_paths:
-            current_section = menu_data['categories']['categories']  # Starting point for traversal
+            current_section = menu_data['categories']  # Starting point for traversal
             for category in path:
                 if category in current_section:
                     current_section = current_section[category]
@@ -211,6 +184,74 @@ def process_data():
         "game_started": game_started,
         "filtered_items": filtered_items
     })
+
+
+def filter_complete_paths(paths):
+    simplified_paths = [[item for item in path if item.lower() != "subcategories"] for path in paths]
+    complete_paths = filter_paths_with_all_ancestors(simplified_paths)
+    terminal_paths = filter_for_terminal_paths(complete_paths)
+
+    return terminal_paths
+
+
+def filter_paths_with_all_ancestors(simplified_paths):
+    paths_as_tuples_set = set(tuple(path) for path in simplified_paths)
+    complete_paths_tuples = [path for path in paths_as_tuples_set if
+                             immediate_ancestor_present(path, paths_as_tuples_set)]
+    complete_paths_lists = [list(path) for path in complete_paths_tuples]
+    return ensure_base_paths(complete_paths_lists)
+
+
+def immediate_ancestor_present(path, all_paths_set):
+    if len(path) == 1:
+        return True
+    immediate_ancestor = path[:-1]
+    return tuple(immediate_ancestor) in all_paths_set
+
+
+def ensure_base_paths(complete_paths_lists):
+    for path in complete_paths_lists:
+        if len(path) > 1:
+            base_path = path[:2]
+            if base_path not in complete_paths_lists:
+                complete_paths_lists.append(base_path)
+    return complete_paths_lists
+
+
+def filter_for_terminal_paths(paths):
+    terminal_paths = [path for path in paths if not any(is_prefix(path, other) for other in paths if path != other)]
+    return terminal_paths
+
+
+def is_prefix(path, other_path):
+    if len(path) >= len(other_path):
+        return False
+    return all(path[i] == other_path[i] for i in range(len(path)))
+
+
+def convert_selection_paths(input_paths):
+    converted_paths = []
+    for path_str in input_paths:
+        # Remove the brackets at the beginning and the end, then split by ', '
+        path_elements = path_str[1:-1].split(", ")
+        # Trim the extra quotes from each element and keep the structure
+        clean_elements = [element.strip("'") for element in path_elements]
+        converted_paths.append(clean_elements)
+    return converted_paths
+
+
+def string_paths_to_lists(string_paths, delimiter='/'):
+    """
+    Convert a list of string paths to a list of lists of keys.
+    """
+    return [path.split(delimiter) for path in string_paths]
+
+
+def paths_to_string(paths, delimiter='/'):
+    """
+    Convert each path in paths to a string using the given delimiter.
+    """
+    return [delimiter.join(path) for path in paths]
 
 
 def find_items(current_section):
